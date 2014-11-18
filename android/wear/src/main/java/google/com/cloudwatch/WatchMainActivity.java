9dc1970012cb127b8f6ffe265bcd60327140f3b7
Nikhil Kothari <nikhilko@google.com>
2014-11-18 09:51:03 -0800

    Formatting fixes

--- android/wear/src/main/java/google/com/cloudwatch/WatchMainActivity.java
+++ android/wear/src/main/java/google/com/cloudwatch/WatchMainActivity.java
@@ -1,3 +1,6 @@
+// WatchMainActivity.java
+//
+
 package google.com.cloudwatch;
 
 import android.content.BroadcastReceiver;
@@ -10,64 +13,63 @@ import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 import android.os.BatteryManager;
 
-import java.util.Calendar;
-
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class WatchMainActivity extends Activity {
 
-    private final static IntentFilter INTENT_FILTER;
-    private final static SimpleDateFormat DATE_FORMAT;
+  private final static IntentFilter INTENT_FILTER;
+  private final static SimpleDateFormat DATE_FORMAT;
 
-    private TextView mTime, mBattery;
+  private TextView mTime, mBattery;
 
-    static {
-        INTENT_FILTER = new IntentFilter();
-        INTENT_FILTER.addAction(Intent.ACTION_TIME_TICK);
-        INTENT_FILTER.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-        INTENT_FILTER.addAction(Intent.ACTION_TIME_CHANGED);
+  static {
+    INTENT_FILTER = new IntentFilter();
+    INTENT_FILTER.addAction(Intent.ACTION_TIME_TICK);
+    INTENT_FILTER.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+    INTENT_FILTER.addAction(Intent.ACTION_TIME_CHANGED);
 
-        DATE_FORMAT = new SimpleDateFormat("kk:mm a");
-    }
+    DATE_FORMAT = new SimpleDateFormat("kk:mm a");
+  }
 
-    private BroadcastReceiver mTimeInfoReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String timeText = DATE_FORMAT.format(Calendar.getInstance().getTime());
-            mTime.setText(timeText);
-        }
-    };
-
-    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            mBattery.setText(String.valueOf(intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0) + "%"));
-        }
-    };
-
+  private BroadcastReceiver mTimeInfoReceiver = new BroadcastReceiver() {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_watch_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTime = (TextView) stub.findViewById(R.id.watch_time);
-                mBattery = (TextView) stub.findViewById(R.id.watch_battery);
-
-                mTimeInfoReceiver.onReceive(null, null);
-                registerReceiver(mTimeInfoReceiver, INTENT_FILTER);
-                registerReceiver(mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-            }
-        });
+    public void onReceive(Context context, Intent intent) {
+      String timeText = DATE_FORMAT.format(Calendar.getInstance().getTime());
+      mTime.setText(timeText);
     }
+  };
 
+  private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
     @Override
-    protected void onDestroy() {
-        unregisterReceiver(mTimeInfoReceiver);
-        unregisterReceiver(mBatInfoReceiver);
-
-        super.onDestroy();
+    public void onReceive(Context context, Intent intent) {
+      mBattery.setText(String.valueOf(intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0) + "%"));
     }
+  };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_watch_main);
+    final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+    stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+      @Override
+      public void onLayoutInflated(WatchViewStub stub) {
+        mTime = (TextView) stub.findViewById(R.id.watch_time);
+        mBattery = (TextView) stub.findViewById(R.id.watch_battery);
+
+        mTimeInfoReceiver.onReceive(null, null);
+        registerReceiver(mTimeInfoReceiver, INTENT_FILTER);
+        registerReceiver(mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      }
+    });
+  }
+
+  @Override
+  protected void onDestroy() {
+    unregisterReceiver(mTimeInfoReceiver);
+    unregisterReceiver(mBatInfoReceiver);
+
+    super.onDestroy();
+  }
 }
