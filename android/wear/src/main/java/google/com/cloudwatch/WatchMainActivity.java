2f654a59b143971fa24a9e64ef37c6c49eb223c5
Nikhil Kothari <nikhilko@google.com>
2014-11-18 10:43:28 -0800

    Cleanup ... build as an app for now

--- android/wear/src/main/java/google/com/cloudwatch/WatchMainActivity.java
+++ android/wear/src/main/java/google/com/cloudwatch/WatchMainActivity.java
@@ -3,73 +3,63 @@
 
 package google.com.cloudwatch;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
-import android.os.BatteryManager;
-
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.support.wearable.view.*;
+import android.widget.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class WatchMainActivity extends Activity {
+public final class WatchMainActivity extends Activity {
 
-  private final static IntentFilter INTENT_FILTER;
-  private final static SimpleDateFormat DATE_FORMAT;
+  private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("kk:mm a");
 
-  private TextView mTime, mBattery;
+  private final BroadcastReceiver _timeInfoReceiver;
+  private TextView _timeLabel;
 
-  static {
-    INTENT_FILTER = new IntentFilter();
-    INTENT_FILTER.addAction(Intent.ACTION_TIME_TICK);
-    INTENT_FILTER.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-    INTENT_FILTER.addAction(Intent.ACTION_TIME_CHANGED);
+  public WatchMainActivity() {
+    super();
 
-    DATE_FORMAT = new SimpleDateFormat("kk:mm a");
+    _timeInfoReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        updateTime();
+      }
+    };
   }
-
-  private BroadcastReceiver mTimeInfoReceiver = new BroadcastReceiver() {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-      String timeText = DATE_FORMAT.format(Calendar.getInstance().getTime());
-      mTime.setText(timeText);
-    }
-  };
-
-  private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-      mBattery.setText(String.valueOf(intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0) + "%"));
-    }
-  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.activity_watch_main);
-    final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+    WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
     stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
       @Override
       public void onLayoutInflated(WatchViewStub stub) {
-        mTime = (TextView) stub.findViewById(R.id.watch_time);
-        mBattery = (TextView) stub.findViewById(R.id.watch_battery);
+        _timeLabel = (TextView)stub.findViewById(R.id.watch_time);
 
-        mTimeInfoReceiver.onReceive(null, null);
-        registerReceiver(mTimeInfoReceiver, INTENT_FILTER);
-        registerReceiver(mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        IntentFilter timeIntentFilter = new IntentFilter();
+        timeIntentFilter.addAction(Intent.ACTION_TIME_TICK);
+        timeIntentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        timeIntentFilter.addAction(Intent.ACTION_TIME_CHANGED);
+
+        registerReceiver(_timeInfoReceiver, timeIntentFilter);
+        updateTime();
       }
     });
   }
 
   @Override
   protected void onDestroy() {
-    unregisterReceiver(mTimeInfoReceiver);
-    unregisterReceiver(mBatInfoReceiver);
-
+    unregisterReceiver(_timeInfoReceiver);
     super.onDestroy();
+  }
+
+  private void updateTime() {
+    String timeText = DATE_FORMAT.format(Calendar.getInstance().getTime());
+    _timeLabel.setText(timeText);
   }
 }
