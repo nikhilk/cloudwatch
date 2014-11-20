b28672e40d7eb1df13faeacf0b83d1de8826e20d
Nikhil Kothari <nikhilko@google.com>
2014-11-20 11:40:32 -0800

    Added custom drawn watch face

---
+++ android/wear/src/main/java/google/com/cloudwatch/WatchFace.java
@@ -0,0 +1,140 @@
+// WatchFace.java
+//
+
+package google.com.cloudwatch;
+
+import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import android.text.format.*;
+import android.view.*;
+
+import java.util.*;
+
+public class WatchFace extends View {
+
+  private Time _time;
+  private String _message;
+
+  private Paint _greenPaint;
+  private Paint _redPaint;
+  private Paint _blackPaint;
+  private Paint _whitePaint;
+
+  public WatchFace(Context context) {
+    super(context);
+
+    _time = new Time(TimeZone.getDefault().getID());
+    _message = "Cloud Watch";
+
+    _whitePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    _whitePaint.setAntiAlias(true);
+    _whitePaint.setStrokeWidth(2);
+    _whitePaint.setStrokeCap(Paint.Cap.BUTT);
+    _whitePaint.setStyle(Paint.Style.STROKE);
+    _whitePaint.setColor(0xffffffff);
+
+    _blackPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    _blackPaint.setAntiAlias(true);
+    _blackPaint.setStrokeWidth(10);
+    _blackPaint.setStrokeCap(Paint.Cap.BUTT);
+    _blackPaint.setStyle(Paint.Style.STROKE);
+    _blackPaint.setColor(0xff000000);
+
+    _redPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    _redPaint.setAntiAlias(true);
+    _redPaint.setStrokeWidth(10);
+    _redPaint.setStrokeCap(Paint.Cap.BUTT);
+    _redPaint.setStyle(Paint.Style.STROKE);
+    _redPaint.setColor(0xff800000);
+
+    _greenPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    _greenPaint.setAntiAlias(true);
+    _greenPaint.setStrokeWidth(4);
+    _greenPaint.setStrokeCap(Paint.Cap.BUTT);
+    _greenPaint.setStyle(Paint.Style.STROKE);
+    _greenPaint.setColor(0xff008000);
+
+    final Handler h = new Handler() {
+      @Override
+      public void handleMessage(Message msg) {
+        updateTime();
+      }
+    };
+
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask()
+    {
+      @Override
+      public void run()
+      {
+        h.sendEmptyMessage(0);
+      }
+    }, 0, 1000);
+  }
+
+  @Override
+  protected void onAttachedToWindow() {
+    super.onAttachedToWindow();
+  }
+
+  @Override
+  protected void onDetachedFromWindow() {
+    super.onDetachedFromWindow();
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+
+    canvas.drawText(_message, 30, 160, _whitePaint);
+
+    // canvas.drawLine(0, 0, 320, 320, _paint);
+    canvas.drawArc(new RectF(10, 10, 310, 310), 135, 270, false, _redPaint);
+    // canvas.drawArc(new RectF(30, 30, 290, 290), 315, 90, false, _blackPaint);
+
+    canvas.drawCircle(160, 160, 140, _whitePaint);
+
+    canvas.save(Canvas.MATRIX_SAVE_FLAG);
+    for (int i = 0; i < 12; i++) {
+      canvas.drawLine(290, 160, 300, 160, _whitePaint);
+      canvas.rotate(30f, 160f, 160f);
+    }
+    canvas.restore();
+
+    canvas.save(Canvas.MATRIX_SAVE_FLAG);
+    canvas.rotate(30 * (_time.hour + _time.minute / 60f) - 90, 160, 160);
+    canvas.drawLine(160, 160, 210, 160, _greenPaint);
+    canvas.restore();
+
+    canvas.save(Canvas.MATRIX_SAVE_FLAG);
+    canvas.rotate(6 * (_time.minute + _time.second / 60f) - 90, 160, 160);
+    canvas.drawLine(160, 160, 250, 160, _greenPaint);
+    canvas.restore();
+
+    canvas.save(Canvas.MATRIX_SAVE_FLAG);
+    canvas.rotate(6 * _time.second - 90, 160, 160);
+    canvas.drawLine(160, 160, 250, 160, _whitePaint);
+    canvas.restore();
+  }
+
+  public void updateMessage(String message) {
+    _message = message;
+    invalidate();
+  }
+
+  public void updateTime() {
+    _time.setToNow();
+
+    int hour = _time.hour;
+    int minute = _time.minute;
+    int second = _time.second;
+
+    invalidate();
+  }
+}
--- android/wear/src/main/java/google/com/cloudwatch/WatchMainActivity.java
+++ android/wear/src/main/java/google/com/cloudwatch/WatchMainActivity.java
@@ -4,10 +4,9 @@
 package google.com.cloudwatch;
 
 import android.app.*;
-import android.content.*;
 import android.os.*;
 import android.support.wearable.view.*;
-import android.widget.*;
+
 import com.fasterxml.jackson.jr.ob.*;
 import com.google.android.gms.common.api.*;
 import com.google.android.gms.wearable.*;
@@ -22,8 +21,7 @@ public final class WatchMainActivity extends Activity
   private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("kk:mm a");
 
   private GoogleApiClient _googleApiClient;
-  private BroadcastReceiver _timeInfoReceiver;
-  private TextView _timeLabel;
+  private WatchFace _watchFace;
 
   @Override
   public void onConnected(Bundle bundle) {
@@ -39,7 +37,6 @@ public final class WatchMainActivity extends Activity
     super.onCreate(savedInstanceState);
 
     initializeGoogleApiClient();
-    registerTimeReceiver();
 
     setContentView(R.layout.activity_watch_main);
     WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
@@ -47,16 +44,14 @@ public final class WatchMainActivity extends Activity
     stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
       @Override
       public void onLayoutInflated(WatchViewStub stub) {
-        _timeLabel = (TextView)stub.findViewById(R.id.watch_time);
-        updateTime();
+        _watchFace = new WatchFace(stub.getContext());
+        stub.addView(_watchFace);
       }
     });
   }
 
   @Override
   protected void onDestroy() {
-    unregisterReceiver(_timeInfoReceiver);
-
     if (_googleApiClient != null) {
       _googleApiClient.unregisterConnectionCallbacks(this);
     }
@@ -74,7 +69,7 @@ public final class WatchMainActivity extends Activity
           Map<String, Object> data = JSON.std.mapFrom(json);
 
           String newText = data.get("timestamp") + ":" + data.get("value");
-          _timeLabel.setText(newText);
+          _watchFace.updateMessage(newText);
         }
         catch (IOException e) {
           e.printStackTrace();
@@ -115,27 +110,5 @@ public final class WatchMainActivity extends Activity
                               .addConnectionCallbacks(this)
                               .build();
     _googleApiClient.connect();
-  }
-
-  private void registerTimeReceiver() {
-    _timeInfoReceiver = new BroadcastReceiver() {
-      @Override
-      public void onReceive(Context context, Intent intent) {
-        updateTime();
-      }
-    };
-
-    IntentFilter timeIntentFilter = new IntentFilter();
-    timeIntentFilter.addAction(Intent.ACTION_TIME_TICK);
-    timeIntentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-    timeIntentFilter.addAction(Intent.ACTION_TIME_CHANGED);
-
-    registerReceiver(_timeInfoReceiver, timeIntentFilter);
-  }
-
-  private void updateTime() {
-    if (_timeLabel != null) {
-      _timeLabel.setText(DATE_FORMAT.format(Calendar.getInstance().getTime()));
-    }
   }
 }
